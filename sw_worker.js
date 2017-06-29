86846eab2a051de1c831389c67220b68e73ddc31
Akshay Ranganath <akrangan@akamai.com>
2017-06-29 12:03:29 -0700

    adding table styles

--- sw_worker.js
+++ sw_worker.js
@@ -5,8 +5,8 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('akshay_blog').then(cache => {
       return cache.addAll([
-        `https://akshayranganath.gihub.io/`,
-        `https://akshayranganath.gihub.io/about`        
+        `/`,
+        `/about`        
       ])
       .then(() => self.skipWaiting());
     })
